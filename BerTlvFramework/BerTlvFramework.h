585a3499e700030921d5c165f28980aafb6e2cf7
Evgeniy Sinev <es@payneteasy.com>
2016-05-13 17:46:48 +0300

    Added all headers to BerTlvFramework.h

--- BerTlvFramework/BerTlvFramework.h
+++ BerTlvFramework/BerTlvFramework.h
@@ -14,6 +14,11 @@ FOUNDATION_EXPORT double BerTlvFrameworkVersionNumber;
 //! Project version string for BerTlvFramework.
 FOUNDATION_EXPORT const unsigned char BerTlvFrameworkVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <BerTlvFramework/PublicHeader.h>
+#import <BerTlv/BerTag.h>
+#import <BerTlv/BerTlv.h>
+#import <BerTlv/BerTlvBuilder.h>
+#import <BerTlv/BerTlvParser.h>
+#import <BerTlv/BerTlvs.h>
+#import <BerTlv/HexUtil.h>
 
 
