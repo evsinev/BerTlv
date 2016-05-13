a2335dc3010be06fcc6605734b94c10bcadb7ddc
Evgeniy Sinev <es@payneteasy.com>
2016-05-13 17:03:02 +0300

    Added BerTlv framework target

---
+++ BerTlvFramework/BerTlvFramework.h
@@ -0,0 +1,19 @@
+//
+//  BerTlvFramework.h
+//  BerTlvFramework
+//
+//  Created by Evgeniy Sinev on 13/05/16.
+//  Copyright © 2016 Evgeniy Sinev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BerTlvFramework.
+FOUNDATION_EXPORT double BerTlvFrameworkVersionNumber;
+
+//! Project version string for BerTlvFramework.
+FOUNDATION_EXPORT const unsigned char BerTlvFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BerTlvFramework/PublicHeader.h>
+
+
