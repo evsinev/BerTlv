9f14f66580fd380abc7255911c4a1bd5f8237429
Evgeniy Sinev <es@payneteasy.com>
2016-05-16 15:14:10 +0300

    Fixed BerTlv.h

--- BerTlv/BerTlv.h
+++ BerTlv/BerTlv.h
@@ -8,8 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-FOUNDATION_EXPORT double OMGHTTPURLRQVersionNumber;
-FOUNDATION_EXPORT const unsigned char OMGHTTPURLRQVersionString[];
+FOUNDATION_EXPORT double BerTlvVersionNumber;
+FOUNDATION_EXPORT const unsigned char BerTlvVersionString[];
 
 @class BerTag;
 
