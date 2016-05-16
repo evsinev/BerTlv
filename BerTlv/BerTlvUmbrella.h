57fa238509828859e28822a3201d7a41e1680014
Evgeniy Sinev <es@payneteasy.com>
2016-05-16 15:19:44 +0300

    Added BerTlv/BerTlvUmbrella.h

---
+++ BerTlv/BerTlvUmbrella.h
@@ -0,0 +1,9 @@
+FOUNDATION_EXPORT double BerTlvVersionNumber;
+FOUNDATION_EXPORT const unsigned char BerTlvVersionString[];
+
+#import <BerTlv/BerTag.h>
+#import <BerTlv/BerTlv.h>
+#import <BerTlv/BerTlvBuilder.h>
+#import <BerTlv/BerTlvParser.h>
+#import <BerTlv/BerTlvs.h>
+#import <BerTlv/HexUtil.h>
